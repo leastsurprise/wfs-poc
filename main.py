97cae97417cfd3356a4da037f8842fc3ebb12aab
leastsurprise <bijou.eminent0y@icloud.com>
2022-06-24 15:47:58 +1200

    trying addressing

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from requests import Request
 import geopandas as gpd
 import re 
 
+my_key = 'dfe042e3ee1445e1be1c7c4daa2f5b2b'
+unaddressed_titles = ['95831','CB9A/258','CB655/91','325158','CB16K/805']
 
-url = 'https://data.linz.govt.nz/services;key=dfe042e3ee1445e1be1c7c4daa2f5b2b/wfs?service=WFS&version=2.0.0'
-params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': 'title_no=95831', 'outputFormat': 'application/json'}
-q = Request('GET', url, params=params).prepare().url
-data = gpd.read_file(q)
-for geom in data['geometry']:
-    # swap lat and lon into the required (in)correct order
-    geom_str = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1', str(geom))
+url = 'https://data.linz.govt.nz/services;key=' + my_key + '/wfs?service=WFS&version=2.0.0'
 
-cql_filter_txt = 'WITHIN(shape,' + str(data.iloc[0]['geometry']) + ')'
-params = {'request': 'GetFeature', 'typeName': 'layer-53353', 'SRSName': 'EPSG:4167', 'cql_filter': cql_filter_txt, 'outputFormat': 'application/json', 'propertyName': 'full_address'}
-q = Request('POST', url, params=params).prepare().url
-data = gpd.read_file(q)
+addressed_titles = {}
+for title_no in unaddressed_titles:
+    addressed_titles[title_no] = []
+    params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': 'title_no=' + title_no, 'outputFormat': 'application/json'}
+    q = Request('GET', url, params=params).prepare().url
+    data = gpd.read_file(q)
+    for geom in data['geometry']:
+        # swap lat and lon into the required (in)correct order
+        geom_str = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1', str(geom))
+        cql_filter_txt = 'WITHIN(shape,' + geom_str + ')'
+        params = {'request': 'GetFeature', 'typeName': 'layer-53353', 'SRSName': 'EPSG:4167', 'cql_filter': cql_filter_txt, 'outputFormat': 'application/json', 'propertyName': 'full_address'}
+        q = Request('POST', url, params=params).prepare().url
+        data = gpd.read_file(q)
+        for address in data['full_address']:
+            addressed_titles[title_no].append(address)
 1 + 1
