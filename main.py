1e8bec736a349aa4faae6b6c250c861aebe41e2b
leastsurprise <bijou.eminent0y@icloud.com>
2022-06-26 19:40:23 +1200

    removed key to json file

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 from requests import Request
 import geopandas as gpd
-import re 
+import re, json 
 
-my_key = 'dfe042e3ee1445e1be1c7c4daa2f5b2b'
-unaddressed_titles = ['95831','CB9A/258','CB655/91','325158','CB16K/805']
+# load config dictionary from json file
+with open('config.json') as f:
+    config = json.load(f)
 
-url = 'https://data.linz.govt.nz/services;key=' + my_key + '/wfs?service=WFS&version=2.0.0'
+
+url = 'https://data.linz.govt.nz/services;key=' + config['my_linz_key'] + '/wfs?service=WFS&version=2.0.0'
 
 addressed_titles = {}
-for title_no in unaddressed_titles:
+for title_no in config['unaddressed_titles']:
     addressed_titles[title_no] = []
     params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': "title_no='" + title_no + "'", 'outputFormat': 'application/json'}
     q = Request('GET', url, params=params).prepare().url
@@ -23,4 +25,15 @@ for title_no in unaddressed_titles:
         data = gpd.read_file(q)
         for address in data['full_address']:
             addressed_titles[title_no].append(address)
-1 + 1
+
+# output the results to the file in config['output_file']
+with open(config['output_file'], 'w') as f:
+    # set the output format to match the extension of config['output_file']
+    if config['output_file'].endswith('.json'):
+        json.dump(addressed_titles, f, indent=4)
+    elif config['output_file'].endswith('.csv'):
+        for title_no in addressed_titles:
+            f.write(title_no + ',' + ','.join(addressed_titles[title_no]) + '\n')
+    else:
+        print('Unknown output file extension')
+        exit(1)
