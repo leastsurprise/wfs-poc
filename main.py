92e93c57d352eb55a6a7cefb141e0057d3bc6d7b
leastsurprise <bijou.eminent0y@icloud.com>
2022-06-29 20:13:07 +1200

    added get_titles_for_addresses

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ url = 'https://data.linz.govt.nz/services;key=' + config['my_linz_key'] + '/wfs?
 
 def get_addresses_for_titles(titles):
     """
-    Takes a list of titles and return them with their addresses (zero to many per title)
+    Takes a list of title numbers and returns them with their addresses (zero to many per title)
     """
     title_address_dict = {}
     # first make wfs calls to get wkt geom for each title
@@ -28,20 +28,20 @@ def get_addresses_for_titles(titles):
 
 def get_title_wkt_geom(title_no):
     """
-    Takes a title and returns a list of coordinates in wkt format, with lon lat swapped to lat lon 
+    Takes a title number and returns the outline of that title as list of lat lon coordinates in wkt format
     """
     global url
     params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': "title_no='" + title_no + "'", 'outputFormat': 'application/json'}
     url_plus_params = Request('GET', url, params=params).prepare().url
     data = gpd.read_file(url_plus_params)
-    1 + 1
-    wkt_outline = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1',  str(data['geometry'][0])) # swap lon lat to lat lon 
+    # the data comes in lon lat order, which is not the format that WFS wants.  So we need to flip the order
+    wkt_outline = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1',  str(data['geometry'][0])) 
     return wkt_outline
 
 
 def get_address_for_wkt_geom(wkt_geom):
     """
-    Takes a wkt_geom and returns an address
+    Takes a title geometry in well known text format and returns all address points that sit within that title geommetry - zero to many.
     """
     global url
     cql_filter_txt = 'WITHIN(shape,' + wkt_geom + ')'
@@ -53,35 +53,52 @@ def get_address_for_wkt_geom(wkt_geom):
          addresses.append(address)
     return addresses
 
+# ---------------------------------
+
+def get_titles_for_addresses(addresses):
+    """
+    Takes a list of addresses and returns them with their titles (zero to many per address)
+    """
+    address_title_dict = {}
+    # first make wfs calls to get wkt geom for each address
+    for address in addresses:
+        address_title_dict[address] = {}
+        address_title_dict[address]['wkt_geom'] = get_address_wkt_geom(address)
+
+    # then make a second call to get the titles for each address point (wkt geom)
+    for address in addresses:
+        address_title_dict[address]['titles'] = get_titles_for_wkt_geom(address_title_dict[address]['wkt_geom'])
+    return address_title_dict
+
+
+def get_address_wkt_geom(address):
+    """
+    Takes a address string and returns the geometric point of that address wkt format
+    """
+    global url
+    params = {'request': 'GetFeature', 'typeName': 'layer-53353', 'SRSName': 'EPSG:4167', 'cql_filter': "full_address='" + address + "'", 'outputFormat': 'application/json'}
+    url_plus_params = Request('GET', url, params=params).prepare().url
+    data = gpd.read_file(url_plus_params)
+    # the data comes in lon lat order, which is not the format that WFS wants.  So we need to flip the order
+    wkt_outline = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1',  str(data['geometry'][0])) 
+    return wkt_outline
+
+
+def get_titles_for_wkt_geom(wkt_geom):
+    """
+    Takes a address geometry in well known text format and returns all titles that encompass that address geommetry - zero to many.
+    """
+    global url
+    cql_filter_txt = 'CONTAINS(shape,' + wkt_geom + ')'
+    params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': cql_filter_txt, 'outputFormat': 'application/json', 'propertyName': 'title_no'}
+    url_plus_params = Request('GET', url, params=params).prepare().url
+    data = gpd.read_file(url_plus_params)
+    titles = []
+    for title in data['title_no']:
+         titles.append(title)
+    return titles
+
 
 if __name__ == "__main__":
-    get_addresses_for_titles(["95831","CB9A/258","CB655/91","325158","CB16K/805"])
-
-
-#addressed_titles = {}
-#for title_no in config['unaddressed_titles']:
-#    addressed_titles[title_no] = []
-#    params = {'request': 'GetFeature', 'typeName': 'layer-50804', 'SRSName': 'EPSG:4167', 'cql_filter': "title_no='" + title_no + "'", 'outputFormat': 'application/json'}
-#    q = Request('GET', url, params=params).prepare().url
-#    data = gpd.read_file(q)
-#    for geom in data['geometry']:
-#        # swap lat and lon into the required (in)correct order
-#        geom_str = re.sub(r'(\d+[.]\d+)\s+([-]\d+[.]\d+)', r'\2 \1', str(geom))
-#        cql_filter_txt = 'WITHIN(shape,' + geom_str + ')'
-#        params = {'request': 'GetFeature', 'typeName': 'layer-53353', 'SRSName': 'EPSG:4167', 'cql_filter': cql_filter_txt, 'outputFormat': 'application/json', 'propertyName': 'full_address'}
-#        q = Request('POST', url, params=params).prepare().url
-#        data = gpd.read_file(q)
-#        for address in data['full_address']:
-#            addressed_titles[title_no].append(address)
-#
-## output the results to the file in config['output_file']
-#with open(config['output_file'], 'w') as f:
-#    # set the output format to match the extension of config['output_file']
-#    if config['output_file'].endswith('.json'):
-#        json.dump(addressed_titles, f, indent=4)
-#    elif config['output_file'].endswith('.csv'):
-#        for title_no in addressed_titles:
-#            f.write(title_no + ',' + ','.join(addressed_titles[title_no]) + '\n')
-#    else:
-#        print('Unknown output file extension')
-#        exit(1)
+    addresses = get_addresses_for_titles(["95831","CB9A/258","CB655/91","325158","CB16K/805"])
+    titles = get_titles_for_addresses(['2 Kajens Court, Lincoln'])
